418da0f82b89a3184a811b643f28b3f12b59e0c7
Liam Tran <liam.tran@laidon.com>
2026-01-19 17:43:54 +0700

    feat: step9 Component configuration

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -8,31 +8,11 @@ import ResourceBundle from "sap/base/i18n/ResourceBundle";
  * @name ui5.walkthrough.controller.App
  */
 export default class AppController extends Controller {
-   onInit(): void {
-      // set data model on view
-      const data = {
-         recipient: {
-            name: "Liam",
-            age: 23,
-            address: "thu duc"
-         }
-      };
-      const dataModel = new JSONModel(data);
-      // because of "strict" mode in tsconfig.json a null check is required for this.getView()
-      this.getView()?.setModel(dataModel);
-
-      // set i18n model on view
-      const i18nModel = new ResourceModel({
-         bundleName: "ui5.walkthrough.i18n.i18n"
-      });
-      this.getView()?.setModel(i18nModel, "i18n");
-   }
    
    onShowHello(): void {
       // read msg from i18n model
       const recipient = (this.getView()?.getModel() as JSONModel)?.getProperty("/recipient/name");
       const age = (this.getView()?.getModel() as JSONModel)?.getProperty("/recipient/age");
-      console.log("age", age)
       const address = (this.getView()?.getModel() as JSONModel)?.getProperty("/recipient/address");
       const resourceBundle = (this.getView()?.getModel("i18n") as ResourceModel)?.getResourceBundle() as ResourceBundle;
       const msg = resourceBundle.getText("helloMsg", [recipient, age, address]) || "no text defined";
--- webapp/index.ts
+++ webapp/index.ts
@@ -1,12 +1,24 @@
-import XMLView from "sap/ui/core/mvc/XMLView";
+// import XMLView from "sap/ui/core/mvc/XMLView";
 
 
 // new Text({
 //     text: "Hello World !!!"
 // }).placeAt("content");
 
-XMLView.create({
-    viewName: "ui5.walkthrough.view.App"
-}).then(function (view) {
-    view.placeAt("content");
-});
+// XMLView.create({
+//     viewName: "ui5.walkthrough.view.App"
+// }).then(function (view) {
+//     view.placeAt("content");
+// });
+
+import ComponentContainer from "sap/ui/core/ComponentContainer";
+
+new ComponentContainer({
+    id: "container",
+    name: "ui5.walkthrough",
+    settings: {
+        id: "walkthrough"
+    },
+    autoPrefixId: true,
+    async: true
+}).placeAt("content");
