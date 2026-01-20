9dc47150595d60113ce179c99f41db29829a916f
Liam Tran <liam.tran@laidon.com>
2026-01-20 18:03:15 +0700

    feat: step 32 routing back and history

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -1,6 +1,7 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
+import History from "sap/ui/core/routing/History";
 
 /**
  * @namespace ui5.walkthrough.controller
@@ -21,5 +22,16 @@ export default class Detail extends Controller {
         ),
       model: "invoice",
     });
+  }
+  onNavBack(): void {
+    const history = History.getInstance();
+    const previousHash = history.getPreviousHash();
+
+    if (previousHash !== undefined) {
+      window.history.go(-1);
+    } else {
+      const router = UIComponent.getRouterFor(this);
+      router.navTo("overview", {}, true);
+    }
   }
 }
